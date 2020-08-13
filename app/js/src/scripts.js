5034253f921b98c4e86a12489ca1deb3704fce2f
Marion Londero <maashhh11@gmail.com>
2020-08-13 10:44:07 +0200

    Mise à jour fichiers + MENU RESPONSIVE

--- app/js/src/scripts.js
+++ app/js/src/scripts.js
@@ -1,8 +1,21 @@
-(function($) {
+// BOUTON RETOUR MENU
 
-	$(document).ready(function() {
+boutonMenu = document.getElementById("boutonMenu");
 
-		
-        
-	});
-})(jQuery);
+// Quand on scroll 200px vers le bas le bouton apparait
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    boutonMenu.style.display = "block";
+  } else {
+    boutonMenu.style.display = "none";
+  }
+}
+
+// Quand on clique sur le bouton retour au début du document
+function retourMenu() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
