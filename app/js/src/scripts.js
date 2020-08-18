a81bcac56afe5082f7381d53cc3c0b35789340d6
Marion Londero <maashhh11@gmail.com>
2020-08-18 14:49:57 +0200

    animation titre slide a jour

--- app/js/src/scripts.js
+++ app/js/src/scripts.js
@@ -1,3 +1,59 @@
+
+//ANIMATION SUR H1
+
+// Faire en sorte que chaque lettre soit un span et appliquer l'annimation
+var textWrapper = document.querySelector('.titre');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>"); //Mon h1 se transforme en span
+
+anime.timeline()
+  .add({
+    targets: '.titre .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  });
+
+
+//ANIMATION SUR H2 titre2
+
+// Faire en sorte que chaque lettre soit un span et appliquer l'annimation
+var textWrapper = document.querySelector('.titre2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>"); //Mon h2 se transforme en span
+
+anime.timeline()
+  .add({
+    targets: '.titre2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  });
+
+
+//ANIMATION SUR H2 titre3
+
+// Faire en sorte que chaque lettre soit un span et appliquer l'annimation
+  var textWrapper = document.querySelector('.titre3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>"); //Mon h2 se transforme en span
+
+anime.timeline()
+  .add({
+    targets: '.titre3 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  });
+
+
+
 // BOUTON RETOUR MENU
 
 boutonMenu = document.getElementById("boutonMenu");
